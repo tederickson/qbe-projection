1b2cadd17c33c393f6e458efacc3c50ca96dadcd
Ted Erickson <ted.erickson@comcast.net>
2026-03-19 10:56:11 -0600

    Testing findById

--- src/main/java/com/erickson/qbe_projection/service/AuthorService.java
+++ src/main/java/com/erickson/qbe_projection/service/AuthorService.java
@@ -4,6 +4,7 @@ import com.erickson.qbe_projection.dto.AuthorResponse;
 import com.erickson.qbe_projection.mapper.AuthorMapper;
 import com.erickson.qbe_projection.model.AuthorEntity;
 import com.erickson.qbe_projection.repository.AuthorRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.Optional;
 public class AuthorService {
     private final AuthorRepository authorRepository;
 
+    @Transactional
     public AuthorResponse findById(Long id) {
         Optional<AuthorEntity> authorEntityOptional = authorRepository.findById(id);
 
--- src/test/java/com/erickson/qbe_projection/service/AuthorServiceIT.java
+++ src/test/java/com/erickson/qbe_projection/service/AuthorServiceIT.java
@@ -1,6 +1,7 @@
 package com.erickson.qbe_projection.service;
 
 import com.erickson.qbe_projection.dto.AuthorResponse;
+import com.erickson.qbe_projection.dto.BookResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 // @Sql("/data/InitializeTests.sql")
@@ -23,14 +25,45 @@ class AuthorServiceIT {
 
     @Test
     void findById_NoBooks() {
-        AuthorResponse authorResponse = authorService.findById(1L);
+        final Long authorId = 1L;
+        AuthorResponse authorResponse = authorService.findById(authorId);
 
-        assertEquals(1L, authorResponse.getAuthorId());
+        assertEquals(authorId, authorResponse.getAuthorId());
         assertEquals("Agatha", authorResponse.getFirstName());
         assertEquals("Christie", authorResponse.getLastName());
         assertEquals("agatha", authorResponse.getUsername());
         assertEquals("agatha@test.net", authorResponse.getEmail());
 
         assertTrue(authorResponse.getBooks().isEmpty());
+    }
+
+    @Test
+    void findById_NoComments() {
+        final Long authorId = 3L;
+        AuthorResponse authorResponse = authorService.findById(authorId);
+
+        assertEquals(authorId, authorResponse.getAuthorId());
+        assertEquals("Jane", authorResponse.getFirstName());
+        assertEquals("Austen", authorResponse.getLastName());
+        assertEquals("jane", authorResponse.getUsername());
+        assertEquals("jane@test.net", authorResponse.getEmail());
+
+        for (BookResponse book : authorResponse.getBooks()) {
+            assertTrue(book.getComments().isEmpty());
+            assertNull(book.getPublishedOn());
+
+            if (book.getBookId() == 4) {
+                assertEquals("Sense and Sensibility", book.getTitle());
+                assertEquals("3 sisters and their widowed mother", book.getContent());
+            }
+            else if (book.getBookId() == 5) {
+                assertEquals("Pride and Prejudice", book.getTitle());
+                assertEquals("Elizabeth Bennet navigates society, family, and marriage", book.getContent());
+            }
+            else {
+                fail(book.toString());
+            }
+        }
+        assertEquals(2, authorResponse.getBooks().size());
     }
 }
