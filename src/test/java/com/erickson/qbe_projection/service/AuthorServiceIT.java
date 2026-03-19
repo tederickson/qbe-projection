d150dc896f50139a4f887178bcd3498de89d053b
Ted Erickson <ted.erickson@comcast.net>
2026-03-19 11:56:23 -0600

    Testing findById

--- src/test/java/com/erickson/qbe_projection/service/AuthorServiceIT.java
+++ src/test/java/com/erickson/qbe_projection/service/AuthorServiceIT.java
@@ -2,13 +2,16 @@ package com.erickson.qbe_projection.service;
 
 import com.erickson.qbe_projection.dto.AuthorResponse;
 import com.erickson.qbe_projection.dto.BookResponse;
+import com.erickson.qbe_projection.dto.CommentResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -68,5 +71,68 @@ class AuthorServiceIT {
             }
         }
         assertEquals(2, authorResponse.getBooks().size());
+    }
+
+    @Test
+    void findById() {
+        final Long authorId = 2L;
+        AuthorResponse authorResponse = authorService.findById(authorId);
+
+        assertEquals(authorId, authorResponse.getAuthorId());
+        assertEquals("Alexandre", authorResponse.getFirstName());
+        assertEquals("Dumas", authorResponse.getLastName());
+        assertEquals("alexandre", authorResponse.getUsername());
+        assertEquals("alex@test.net", authorResponse.getEmail());
+
+        for (BookResponse book : authorResponse.getBooks()) {
+            if (book.getBookId() == 1) {
+                assertEquals("The Three Musketeers", book.getTitle());
+                assertEquals("4 guys save the Queen", book.getContent());
+                assertEquals(LocalDate.parse("1844-01-25"), book.getPublishedOn());
+                assertTrue(book.getComments().isEmpty());
+            }
+            else if (book.getBookId() == 2) {
+                assertEquals("The Count of Monte Cristo", book.getTitle());
+                assertEquals("The victim of betrayal by friends", book.getContent());
+                assertEquals(LocalDate.parse("1844-08-05"), book.getPublishedOn());
+                assertTrue(book.getComments().isEmpty());
+            }
+            else if (book.getBookId() == 3) {
+                assertEquals("The Man in the Iron Mask", book.getTitle());
+                assertEquals("The Musketeers replace the cruel King with his twin", book.getContent());
+                assertEquals(LocalDate.parse("1847-12-05"), book.getPublishedOn());
+
+                for (CommentResponse comment : book.getComments()) {
+                    System.out.println("comment = " + comment);
+                    switch (comment.getCommentId().intValue()) {
+                        case 1 -> {
+                            assertEquals("george", comment.getName());
+                            assertEquals("Such a twist!", comment.getContent());
+                            assertEquals(LocalDateTime.parse("1944-11-25T00:00"), comment.getPublishedOn());
+                            assertNull(comment.getUpdatedOn());
+                        }
+                        case 2 -> {
+                            assertEquals("carl", comment.getName());
+                            assertEquals("The victim of betrayal by his own brother", comment.getContent());
+                            assertEquals(LocalDateTime.parse("1962-08-05T00:00"), comment.getPublishedOn());
+                            assertNotNull(comment.getUpdatedOn());
+                        }
+                        case 3 -> {
+                            assertEquals("roberto", comment.getName());
+                            assertEquals("The Musketeers ride again", comment.getContent());
+                            assertEquals(LocalDateTime.parse("2022-12-05T00:00"), comment.getPublishedOn());
+                            assertNotNull(comment.getUpdatedOn());
+                        }
+
+                        default -> fail(comment.toString());
+                    }
+                }
+                assertEquals(3, book.getComments().size());
+            }
+            else {
+                fail(book.toString());
+            }
+        }
+        assertEquals(3, authorResponse.getBooks().size());
     }
 }
